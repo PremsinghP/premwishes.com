6d958645872d416e6c27b3f7cc4cfc2a6e186c39
Premsingh Padya <32636735+PremsinghP@users.noreply.github.com>
2023-09-15 22:09:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-// Set the date of your birthday (in this example, it's December 31, 2023, at 00:00:00 UTC)
-const birthdayDate = new Date("2023-12-31T00:00:00").getTime();
+// Set the date of Aliya's birthday (September 16, 2023, at 00:00:00 UTC)
+const birthdayDate = new Date("2023-09-16T00:00:00").getTime();
 
-// Update the countdown every second
-const countdown = setInterval(function() {
+// Function to update the countdown
+function updateCountdown() {
     const currentDate = new Date().getTime();
     const timeLeft = birthdayDate - currentDate;
 
     if (timeLeft <= 0) {
         clearInterval(countdown);
-        document.getElementById("countdown").innerHTML = "<p>Happy Birthday!</p>";
+        document.getElementById("countdown").innerHTML = "<p>Happy Birthday Aliya!</p>";
     } else {
         const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
         const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -20,7 +20,13 @@ const countdown = setInterval(function() {
         document.getElementById("minutes").innerHTML = formatTime(minutes);
         document.getElementById("seconds").innerHTML = formatTime(seconds);
     }
-}, 1000);
+}
+
+// Update the countdown immediately
+updateCountdown();
+
+// Check and update the countdown every second
+const countdown = setInterval(updateCountdown, 1000);
 
 // Function to format time as "00" for single digits
 function formatTime(time) {
