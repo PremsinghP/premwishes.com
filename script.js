260d3f29819caae4db6765df799b44d2e4d68754
Premsingh Padya <32636735+PremsinghP@users.noreply.github.com>
2023-09-15 22:06:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Set the date of your birthday
+// Set the date of your birthday (in this example, it's December 31, 2023, at 00:00:00 UTC)
 const birthdayDate = new Date("2023-12-31T00:00:00").getTime();
 
 // Update the countdown every second
@@ -6,20 +6,19 @@ const countdown = setInterval(function() {
     const currentDate = new Date().getTime();
     const timeLeft = birthdayDate - currentDate;
 
-    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
-
-    document.getElementById("days").innerHTML = formatTime(days);
-    document.getElementById("hours").innerHTML = formatTime(hours);
-    document.getElementById("minutes").innerHTML = formatTime(minutes);
-    document.getElementById("seconds").innerHTML = formatTime(seconds);
-
-    // If the countdown is over, display a message
-    if (timeLeft < 0) {
+    if (timeLeft <= 0) {
         clearInterval(countdown);
         document.getElementById("countdown").innerHTML = "<p>Happy Birthday!</p>";
+    } else {
+        const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+        document.getElementById("days").innerHTML = formatTime(days);
+        document.getElementById("hours").innerHTML = formatTime(hours);
+        document.getElementById("minutes").innerHTML = formatTime(minutes);
+        document.getElementById("seconds").innerHTML = formatTime(seconds);
     }
 }, 1000);
 
