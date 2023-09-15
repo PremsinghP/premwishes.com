f0de0fa74411010b8a4432cecd7dd39f2368ad57
Premsingh Padya <32636735+PremsinghP@users.noreply.github.com>
2023-09-16 00:05:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,19 +12,31 @@ function updateCountdown() {
     }
 
     const timeLeft = birthdayDate - currentDate;
-    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+    if (timeLeft <= 0) {
+        // Birthday is today or in the past
+        document.getElementById("countdown").innerHTML = `<p>Birthday was ${Math.abs(daysBetween(currentDate, birthdayDate))} days ago</p>`;
+    } else {
+        // Countdown to birthday
+        const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
-    document.getElementById("days").textContent = formatTime(days);
-    document.getElementById("hours").textContent = formatTime(hours);
-    document.getElementById("minutes").textContent = formatTime(minutes);
-    document.getElementById("seconds").textContent = formatTime(seconds);
+        document.getElementById("days").textContent = formatTime(days);
+        document.getElementById("hours").textContent = formatTime(hours);
+        document.getElementById("minutes").textContent = formatTime(minutes);
+        document.getElementById("seconds").textContent = formatTime(seconds);
+    }
 }
 
 function formatTime(time) {
     return time < 10 ? `0${time}` : time;
+}
+
+function daysBetween(date1, date2) {
+    // Calculate the difference in days between two dates
+    const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+    return Math.floor(Math.abs((date1 - date2) / oneDay));
 }
 
 updateCountdown();
