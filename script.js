2de713feb33fef87163aa8ea039ffd869f546d87
Premsingh Padya <32636735+PremsinghP@users.noreply.github.com>
2023-09-15 21:56:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,29 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const form = document.getElementById("birthdayForm");
-    const wishesList = document.getElementById("wishesList");
+// Set the date of your birthday
+const birthdayDate = new Date("2023-12-31T00:00:00").getTime();
 
-    form.addEventListener("submit", function (event) {
-        event.preventDefault();
+// Update the countdown every second
+const countdown = setInterval(function() {
+    const currentDate = new Date().getTime();
+    const timeLeft = birthdayDate - currentDate;
 
-        // Get user input
-        const nameInput = document.getElementById("name");
-        const messageInput = document.getElementById("message");
+    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
-        const name = nameInput.value;
-        const message = messageInput.value;
+    document.getElementById("days").innerHTML = formatTime(days);
+    document.getElementById("hours").innerHTML = formatTime(hours);
+    document.getElementById("minutes").innerHTML = formatTime(minutes);
+    document.getElementById("seconds").innerHTML = formatTime(seconds);
 
-        // Create a new birthday wish element
-        const wishElement = document.createElement("div");
-        wishElement.classList.add("birthday-wish");
-        wishElement.innerHTML = `
-            <strong>${name} says:</strong>
-            <p>${message}</p>
-        `;
-
-        // Add the wish to the list
-        wishesList.appendChild(wishElement);
-
-        // Clear the form inputs
-        nameInput.value = "";
-        messageInput.value = "";
-    });
-
-    // Function to clear birthday wishes after 24 hours
-    function clearWishes() {
-        wishesList.innerHTML = ""; // Clear the wishes list
+    // If the countdown is over, display a message
+    if (timeLeft < 0) {
+        clearInterval(countdown);
+        document.getElementById("countdown").innerHTML = "<p>Happy Birthday!</p>";
     }
+}, 1000);
 
-    // Set a timer to clear wishes after 24 hours (24 * 60 * 60 * 1000 milliseconds)
-    setInterval(clearWishes, 24 * 60 * 60 * 1000);
-});
+// Function to format time as "00" for single digits
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
